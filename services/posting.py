aa596ca08c8a4473d8a1f8880ecb2d4d43dac93e
Artur <petrikartur@gmail.com>
2023-07-14 16:03:15 +0300

    Fixed status 429 too many telegram api calls

--- services/posting.py
+++ services/posting.py
@@ -14,23 +14,34 @@ URL_EDIT_MESSAGE = f"https://api.telegram.org/bot{API_TOKEN}/editMessageCaption"
 URL_SEND_MEDIA = f"https://api.telegram.org/bot{API_TOKEN}/sendMediaGroup"
 
 
+def repeat_request_on_failure(func):
+    def wrapper_repeat_request(*args, **kwargs):
+        response = func(*args, **kwargs)
+        while response.status_code != 200:
+            response = func(*args, **kwargs)
+        return response
+
+    return wrapper_repeat_request
+
+
+@repeat_request_on_failure
+def send_post_request(url: str, payload: dict) -> requests.Response:
+    return requests.post(url, json=payload)
+
+
 def create_post(images: list[str], caption: str, parse_mode: str = "MarkdownV2") -> int:
     photos = [{"type": "photo", "media": image} for image in images]
-    photos[0].update(
-        caption=caption, parse_mode=parse_mode
-    )  # a trick to set a caption for entire post
-    response = requests.post(URL_SEND_MEDIA, json={"chat_id": CHANNEL_ID, "media": photos})
-    while response.status_code == 429:
-        response = requests.post(URL_SEND_MEDIA, json={"chat_id": CHANNEL_ID, "media": photos})
+    photos[0].update(caption=caption, parse_mode=parse_mode)  # set a caption for entire post
+    response = send_post_request(URL_SEND_MEDIA, {"chat_id": CHANNEL_ID, "media": photos})
     data = response.json()
     post_id = data["result"][0]["message_id"]
     return post_id
 
 
 def edit_post_caption(post_id: int, caption: str, parse_mode: str = "MarkdownV2") -> None:
-    requests.post(
+    send_post_request(
         URL_EDIT_MESSAGE,
-        json={
+        {
             "chat_id": CHANNEL_ID,
             "message_id": post_id,
             "caption": caption,
@@ -40,9 +51,9 @@ def edit_post_caption(post_id: int, caption: str, parse_mode: str = "MarkdownV2"
 
 
 def reply_to_post(post_id: int, text: str, parse_mode: str = "MarkdownV2") -> None:
-    requests.post(
+    send_post_request(
         URL_SEND_MESSAGE,
-        json={
+        {
             "chat_id": CHANNEL_ID,
             "text": text,
             "parse_mode": parse_mode,
